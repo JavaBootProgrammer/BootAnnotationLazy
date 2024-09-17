434f1b11fdbcb8bb0afe7bb265cd079ea186598d
anand8533 <161494555+anand8533@users.noreply.github.com>
2024-09-17 11:50:29 -0400

    11

--- src/main/java/com/google/BootAnnotationLazyApplication.java
+++ src/main/java/com/google/BootAnnotationLazyApplication.java
@@ -3,16 +3,21 @@ package com.google;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class BootAnnotationLazyApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(BootAnnotationLazyApplication.class, args);
 
         ApplicationContext context = SpringApplication.run(BootAnnotationLazyApplication.class, args);
-        PersonSettingsPage bean= (PersonSettingsPage)context.getBean("PersonSettingsPage");
-        System.out.println(bean.getFirstName());
+        PersonSettingsPage bean= (PersonSettingsPage)context.getBean("date");
+        System.out.println(bean.getDate());
 
 
 
--- src/main/java/com/google/PersonSettingsPage.java
+++ src/main/java/com/google/PersonSettingsPage.java
@@ -1,12 +1,21 @@
 package com.google;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+@Configuration
 @Component("PersonSettingsPage")
 public class PersonSettingsPage {
     private String firstName="ram";
-    private String lastName;
+    private String lastName="ra";
+    private String email="an";
 
     public String getFirstName() {
         return firstName;
@@ -20,7 +29,11 @@ public class PersonSettingsPage {
         return email;
     }
 
-    private String email;
+    @Bean("date")
+    public Date getDate(){
+        return new Date();
+    }
+
 
     public PersonSettingsPage(String firstName, String lastName, String email) {
         System.out.println("param constructor");
@@ -28,6 +41,7 @@ public class PersonSettingsPage {
         this.lastName = lastName;
         this.email = email;
     }
+//    @Autowired
     public PersonSettingsPage(){
         System.out.println("default constructor");
     }
